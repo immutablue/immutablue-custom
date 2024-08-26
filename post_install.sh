51944cf5fc63d451522790b2400e68bdf3a23f82
Zach Podbielniak <z.podbielniak@disroot.org>
2024-08-26 09:20:16 -0400

    chore: fix type-o in post-install.sh and add example reference

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-# Run any post_install stuff you want here. 
-# This is ran at the end of `immutablue install` and `immutablue update
+# Run any post_install stuff you want here. For an example of what you might want
+# - https://gitlab.com/immutablue/hyacinth-macaw/-/blob/master/post_install.sh
+# This is ran at the end of `immutablue install` and `immutablue update`
 
-`
+
