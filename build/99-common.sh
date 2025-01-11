3d32eaf6170da4501a56fe6e019a8923475fb602
Zach Podbielniak <z.podbielniak@disroot.org>
2025-01-10 23:36:52 -0500

    feat(build): add package by build support

--- build/99-common.sh
+++ build/99-common.sh
@@ -11,7 +11,37 @@ get_yaml_array() {
     local key="$1"
     for yaml in ${PACKAGES_YAMLS}
     do
-        cat <(yq "$key[]" < "${yaml}") <(yq "${key}_${MARCH}[]" < "${yaml}")
+        cat <(yq "${key}[]" < "${yaml}") <(yq "${key}_${MARCH}[]" < "${yaml}")
+
+        if [[ "$(immutablue_build_is_nucleus)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_nucleus[]" < "${yaml}") <(yq "${key}_nucleus_${MARCH}[]" < "${yaml}")
+        fi
+
+        if [[ "$(immutablue_build_is_kuberblue)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_kuberblue[]" < "${yaml}") <(yq "${key}_kuberblue_${MARCH}[]" < "${yaml}")
+        fi
+        
+        if [[ "$(immutablue_build_is_trueblue)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_trueblue[]" < "${yaml}") <(yq "${key}_trueblue_${MARCH}[]" < "${yaml}")
+        fi
+
+        if [[ "$(immutablue_build_is_lts)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_lts[]" < "${yaml}") <(yq "${key}_lts_${MARCH}[]" < "${yaml}")
+        fi
+
+        if [[ "$(immutablue_build_is_cyan)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_cyan[]" < "${yaml}") <(yq "${key}_cyan_${MARCH}[]" < "${yaml}")
+        fi
+
+        if [[ "$(immutablue_build_is_asahi)" == "${TRUE}" ]]
+        then
+            cat <(yq "${key}_asahi[]" < "${yaml}") <(yq "${key}_asahi_${MARCH}[]" < "${yaml}")
+        fi
     done
 }
 
